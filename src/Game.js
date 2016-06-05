4800f6c791c0e078b2c7fb0b7ea07306dd841725
JS1 <joesantoru@gmail.com>
2016-06-05 15:23:38 +0000

    added xwing config, and added to board

--- src/Game.js
+++ src/Game.js
@@ -51,21 +51,27 @@ Game.prototype.initialize = function() {
     
     // add the ties to the board
     var tie1 = new Ship("tie-fo-fighter", "tie1");
-    tie1.addToBoard(435, 860);
+    tie1.addToBoard(435, 760);
     
     var tie2 = new Ship("tie-fo-fighter", "tie2");
-    tie2.addToBoard(500, 850);
+    tie2.addToBoard(500, 750);
     
     var tie3 = new Ship("tie-fo-fighter", "tie3");
-    tie3.addToBoard(445, 920);
+    tie3.addToBoard(445, 820);
     
     var tie4 = new Ship("tie-fo-fighter", "tie4");
-    tie4.addToBoard(510, 910);
+    tie4.addToBoard(510, 810);
+    
+    // add the xwing to the board, facing down
+    var xwing1 = new Ship("t70-xwing", "xwing1");
+    xwing1.addToBoard(475, 100);
+    xwing1.turn(180);
     
     this.ships.push(tie1);
     this.ships.push(tie2);
     this.ships.push(tie3);
     this.ships.push(tie4);
+    this.ships.push(xwing1);
 }
 
 Game.prototype.addTemplateToBoard = function() {
