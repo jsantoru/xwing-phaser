b1a439a24f2e77f924657300e2c78ae78c8d499f
jsantoru <joesantoru@gmail.com>
2016-06-10 21:53:15 -0400

    Feature/set default phase (#23)
    * set default phase to planning phase
    
    * renamed set default to init()

--- src/Phases.js
+++ src/Phases.js
@@ -2,10 +2,18 @@ var Phases = function() {
     
     var selectedPhase = "";
     
+    this.init();
+    
     $('.phase').click( function() {
         $('.phase').removeClass("active");
         $(this).addClass("active");
         
         selectedPhase = $(this).text();
     });
+}
+
+Phases.prototype.init = function() {
+    var $planningPhase = $('#planningPhase');
+    $planningPhase.addClass("active");
+    this.selectedPhase = $planningPhase.text();
 }
